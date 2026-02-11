554d9c6bd0be94a729694556802ac099ef4579e2
pg399 <pg399@kent.ac.uk>
2026-02-11 11:07:44 +0000

    cxfvdssf

--- part_1.py
+++ part_1.py
@@ -1,1 +1,16 @@
+sample_bay = [ "Basalt", "Silica", "Iron", "Dust"]
+print(sample_bay[0]) #Printing 1st item in list
+print(sample_bay[-1]) #'-1' > Negative indexing
+print(len(sample_bay)) #Printing total no of samples
 
+for sample in sample_bay:
+    print(f"Transmitting data for: {sample}")
+
+new_findings = []
+for i in range(3):
+    rock = input ("Enter rock: ")
+print(new_findings)
+
+if "Dust" in sample_bay:
+    sample_bay.remove("Dust")
+print(sample_bay)
--- part_2.py
+++ part_2.py
@@ -1,1 +1,14 @@
+rover_status = {
+    "Battery": 100,
+    "Heater" : "Off",
+    "Camera" : "Standby"
+}
+print(rover_status["Battery"])
+
+rover_status["Battery"] = 85
+rover_status["Heater"]  = "On"
+rover_status["Speed"]   =  5
+print(rover_status)
+
+mission_log 
 
